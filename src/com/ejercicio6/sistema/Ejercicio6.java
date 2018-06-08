2ebfe06e31ca66296e3266cb2177693ac1983c90
marcobonilla <marco.bonilla.alvarado@gmail.com>
2018-06-08 12:58:44 -0600

    Ejercicio6 Ya se ejecuta, creacion Principal

--- src/com/ejercicio6/sistema/Ejercicio6.java
+++ src/com/ejercicio6/sistema/Ejercicio6.java
@@ -1,33 +1,67 @@
 
 package com.ejercicio6.sistema;
 
+import com.ejercicio6.controller.LoginController;
+import java.io.IOException;
+import java.io.InputStream;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author USER
- */
-public class Ejercicio6 extends Application {
+public class Ejercicio6 extends Application {    
     
+    private Stage escenarioPrincipal;
+    private final String PAQUETE_VISTA = "/com/ejercicio6/view/";
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+    
+    public void start(Stage escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
+        this.escenarioPrincipal.setTitle("Ejercicio 6");
+      
+        mostrarLogin();
+        this.escenarioPrincipal.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    public void mostrarLogin(){
+        try{
+            LoginController login = (LoginController)cambiarEscena("LoginView.fxml",600,400);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public Initializable cambiarEscena(String fxml, int ancho, int alto) throws IOException{
+        Initializable resultado = null;
+        
+        FXMLLoader cargadorFXML = new FXMLLoader();
+        
+        InputStream archivo = Ejercicio6.class.getResourceAsStream(PAQUETE_VISTA + fxml);
+        
+        cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
+        
+        cargadorFXML.setLocation(Ejercicio6.class.getResource(PAQUETE_VISTA + fxml));        
+        
+        Scene escena = new Scene((AnchorPane)cargadorFXML.load(archivo),ancho,alto); 
+       
+        this.escenarioPrincipal.setScene(escena);
+        
+        this.escenarioPrincipal.sizeToScene();
+       
+        resultado = (Initializable)cargadorFXML.getController();
+        return resultado;
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
